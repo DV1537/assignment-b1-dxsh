0b78a4692850e8160d5e3b9386fd5b691ad7d8ae
Darwin Shameran <dxsh@riseup.net>
2018-12-04 20:05:31 +0100

    Implement member function addShape and private function realloc

--- Figure.cpp
+++ Figure.cpp
@@ -1,6 +1,37 @@
 #include "Figure.h"
 
-Figure::addShape(std::string* s) {
+void Figure::realloc() {
+    this->shapes++;
+    auto tmp = new std::string*[this->shapes]();
+
+    for (int i = 0; i < this->shapes - 1; i++) {
+        tmp[i] = this->ppShapes[i];
+    }
+    
+    delete[] this->ppShapes;
+    this->ppShapes = tmp;
+}
+
+void Figure::addShape(std::string* s) {
+    realloc();
+
+    ppShapes[this->shapes - 1] = s;
+    for (int i = 0; i < this->shapes; i++) {
+        std::cout << "len: " << this->ppShapes[i]->size() << '\n';
+    }
+}
+
+
+int main() {
+    Figure figure;
+    std::string foo = ("foo");
+
+    figure.addShape(&foo);
 
 }
-/* vim: set ts=2 sw=2 tw=79 ft=cpp et :*/
+
+
+Figure::~Figure() {
+    delete[] this->ppShapes;
+}
+/* vim: set ts=4 sw=4 tw=79 ft=cpp et :*/
--- Figure.h
+++ Figure.h
@@ -1,15 +1,19 @@
 #ifndef FIGURE_H
 #define FIGURE_H
 #include <string>
+#include <iostream>
 
 class Figure {
  private:
      std::string** ppShapes;    /*  placeholder for Shapes obj */
      unsigned int shapes;
+     void realloc();
 
  public:
-     Figure() : ppShapes(nullptr), shapes(0) {}
-     ~Figure() {};
+     Figure() : ppShapes{nullptr}, shapes{0} {}
+     ~Figure();
+     void addShape(std::string* s);
+     std::string getBoundingBox() const;
 };
 
 #endif
